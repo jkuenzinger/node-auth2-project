46a589d15ee0ac4bb74ae483e6ab53725c657f92
jkuenzinger <jkuenzinger228@gmail.com>
2020-04-21 08:31:35 -0700

    set up knexfile everything good to go to create endponits tables and authentication

--- knexfile.js
+++ knexfile.js
@@ -4,26 +4,15 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/auth.db3'
+    },
+    migrations: {
+      directory: './data/migrations',
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
 
   production: {
     client: 'postgresql',
